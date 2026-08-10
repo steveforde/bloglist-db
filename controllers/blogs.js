c6452ffa7443dc2e42527c1831a75c29f110e98e
Steve Forde <sforde08@gmail.com>
2026-08-10 20:32:02 +0100

    Refactor application into modular structure

---
+++ controllers/blogs.js
@@ -0,0 +1,41 @@
+const router = require('express').Router()
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  if (!req.blog) {
+    return res.status(404).end()
+  }
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error: error.message })
+  }
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  res.json(req.blog)
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  await req.blog.destroy()
+  res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  req.blog.likes = req.body.likes
+  await req.blog.save()
+  res.json(req.blog)
+})
+
+module.exports = router
