9ca422918494cc8f729417ae17552b727bc5e4d5
Steve Forde <sforde08@gmail.com>
2026-08-11 14:12:49 +0100

    Complete Exercise 8 - Let there be users

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const { User } = require('../models')
+
+router.get('/', async (req, res) => {
+  const users = await User.findAll()
+  res.json(users)
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const { username, name } = req.body
+    const user = await User.create({ username, name })
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:username', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.params.username }
+    })
+
+    if (user) {
+      user.name = req.body.name
+      await user.save()
+      res.json(user)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express()
 const { PORT } = require('./util/config')
 const { connectToDatabase } = require('./util/db')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users') // ADD THIS
 const { errorHandler } = require('./util/middleware')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter) // ADD THIS
 
-app.use(errorHandler) // ADD THIS: Must be loaded after the routes
+app.use(errorHandler)
 
 const start = async () => {
   await connectToDatabase()
