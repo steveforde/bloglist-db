4168b052279b3d1aa82ec22d51cbf3f606eb6ba2
Steve Forde <sforde08@gmail.com>
2026-08-16 19:26:08 +0100

    Fix model synchronization order and await table creation

--- models/index.js
+++ models/index.js
@@ -4,11 +4,14 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+const syncModels = async () => {
+  await User.sync({ alter: true })
+  await Blog.sync({ alter: true })
+}
 
-Blog.sync({ alter: true })
-User.sync({ alter: true })
+syncModels()
 
 module.exports = {
   Blog,
-  User
+  User,
 }
