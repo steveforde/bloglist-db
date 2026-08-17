1271ba7a2c7349d842f378bcc309d1d497167bca
Steve Forde <sforde08@gmail.com>
2026-08-17 16:21:21 +0100

    Complete migration setup with Umzug

--- models/index.js
+++ models/index.js
@@ -4,13 +4,6 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-const syncModels = async () => {
-  await User.sync({ alter: true })
-  await Blog.sync({ alter: true })
-}
-
-syncModels()
-
 module.exports = {
   Blog,
   User,
--- util/db.js
+++ util/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL, TEST_DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 
 const url = process.env.TESTING === 'true' 
   ? TEST_DATABASE_URL 
@@ -16,9 +17,26 @@ const sequelize = new Sequelize(url, useSSL ? {
   }
 } : {})
 
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: 'migrations/*.js',
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  })
+  
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name),
+  })
+}
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     console.log('connected to the database')
   } catch (err) {
     console.log('failed to connect to the database', err)
