229b9d95a435e4e55ad44c211c42da28c7cbdab5
Steve Forde <sforde08@gmail.com>
2026-08-18 11:43:19 +0100

    Add reading list migration, model, and associations (Exercise 20)

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./reading_list')
 
-User.hasMany(Blog)
 Blog.belongsTo(User)
+User.hasMany(Blog)
+
+User.belongsToMany(Blog, { through: ReadingList, as: 'reading_lists' })
+Blog.belongsToMany(User, { through: ReadingList, as: 'reading_users' })
 
 module.exports = {
-  Blog,
-  User,
+  Blog, User, ReadingList
 }
